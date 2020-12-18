b6fad10262d7855967f8c8c81fd9af51bd0280fd
willyqin <qinwei206@gmail.com>
2020-12-18 17:05:23 +0800

    test cifar10

---
+++ test_code.py
@@ -0,0 +1,17 @@
+import os
+import torch
+import torchvision
+
+import cifar10
+import argparse
+
+def test_cifar10():
+    parser = argparse.ArgumentParser(description='test')
+    args = parser.parse_args()
+    args.noise_type = 'asym'
+    args.noise_rate = 0.4
+
+    train_dataset, val_dataset, train_Cval_dataset, train_Nval_dataset = cifar10.get_cifar10_train(root = './data', args=args,train=True,download=True)
+
+    print(train_dataset.get_noisy_label_acc())
+    print(train_dataset.get_soft_labels_acc())
