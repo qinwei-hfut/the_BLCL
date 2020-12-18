22d09c536518e499ca46b84612c26270721befbf
willyqin <qinwei206@gmail.com>
2020-12-18 20:23:51 +0800

    nr=0.8

--- test_code.py
+++ test_code.py
@@ -11,7 +11,7 @@ def test_cifar10():
     parser = argparse.ArgumentParser(description='test')
     args = parser.parse_args()
     args.noise_type = 'sym'
-    args.noise_rate = 0.4
+    args.noise_rate = 0.8
 
     train_dataset, val_dataset, train_Cval_dataset, train_Nval_dataset = cifar10.get_cifar10_train(root = './data', args=args,train=True,download=True)
 
