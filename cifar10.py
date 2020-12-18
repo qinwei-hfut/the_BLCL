ca77ebe7c28dcb6848b6f0612ba5b9a3a0bec427
willyqin <qinwei206@gmail.com>
2020-12-18 17:04:44 +0800

    添加注释

--- cifar10.py
+++ cifar10.py
@@ -18,6 +18,11 @@ def get_cifar10_train(root, args, train=True,
     val_dataset = CIFAR10_train(root, val_idxs, val_indices=None, args=args, train=train, transform=transform)
     train_Cval_dataset = CIFAR10_train(root, train_idxs,val_idxs, args, train=train, transform=transform)
 
+    # train_dataset:仅仅45K的noisy trainset(但是noisy label; gt label; soft label都输出)
+    # val_dataset: 5K的noisy val set，同样，由于同时输出noisy label，gt label，soft label，可以既满足clean val进行val，可以满足使用noisy val进行val
+    # train_Nval_dataset：50K张图片都输出，同样也输出noisy label, gt label, soft label;但是需要注意的是被包含的val的noisy label其实是noisy的
+    # train_Cval_dataset: 50K张图片都输出，同样也输出noisy label，gt label，soft label；但是需要注意的是被包含的val的noisy label其实都是clean的
+    # 注意：这些dataset的train和val的分割是一样的；但是，noisy label examples的选择肯定是不一样的；
     return train_dataset, val_dataset, train_Cval_dataset,train_Nval_dataset
     
 
