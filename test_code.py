6fe4309e91ad4ab4d4a99ca2c679fe3258741199
willyqin <qinwei206@gmail.com>
2020-12-18 17:09:56 +0800

    set_trace

--- test_code.py
+++ test_code.py
@@ -5,6 +5,8 @@ import torchvision
 import cifar10
 import argparse
 
+import pdb
+
 def test_cifar10():
     parser = argparse.ArgumentParser(description='test')
     args = parser.parse_args()
@@ -13,5 +15,8 @@ def test_cifar10():
 
     train_dataset, val_dataset, train_Cval_dataset, train_Nval_dataset = cifar10.get_cifar10_train(root = './data', args=args,train=True,download=True)
 
+    print('1')
     print(train_dataset.get_noisy_label_acc())
     print(train_dataset.get_soft_labels_acc())
+
+    pdb.set_trace()
