05d098bc87cacbc4655de416368aedda10398c63
willyqin <qinwei206@gmail.com>
2020-12-18 19:35:57 +0800

    noise type

--- test_code.py
+++ test_code.py
@@ -10,12 +10,11 @@ import pdb
 def test_cifar10():
     parser = argparse.ArgumentParser(description='test')
     args = parser.parse_args()
-    args.noise_type = 'asym'
+    args.noise_type = 'sym'
     args.noise_rate = 0.4
 
     train_dataset, val_dataset, train_Cval_dataset, train_Nval_dataset = cifar10.get_cifar10_train(root = './data', args=args,train=True,download=True)
 
-    print('1')
     print(train_dataset.get_noisy_label_acc())
     print(train_dataset.get_soft_labels_acc())
 
