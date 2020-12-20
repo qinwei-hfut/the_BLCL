647e3be0e3827ffee4fa540462d5fdf9ff692ad7
willyqin <qinwei206@gmail.com>
2020-12-20 20:12:48 +0800

    loss bug

--- loss_functions.py
+++ loss_functions.py
@@ -8,10 +8,10 @@ def soft_ce_loss(output, soft_target):
     return -torch.mean(torch.sum(F.log_softmax(output, dim=1) * soft_target, dim=1))
 
 def MAE_loss(output, target):
-    target = torch.zeros(len(target), 10).scatter_(1, target.view(-1,1), 1)
+    target = torch.zeros(len(target), 10).cuda().scatter_(1, target.view(-1,1), 1)
     return torch.nn.L1Loss(output,target)
 
 def MSE_loss(output,target):
-    target = torch.zeros(len(target), 10).scatter_(1, target.view(-1,1), 1)
+    target = torch.zeros(len(target), 10).cuda().scatter_(1, target.view(-1,1), 1)
     return F.mse_loss(output,target)
 
