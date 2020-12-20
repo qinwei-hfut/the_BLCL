7ee717302f476bf9d1a936c77bed2e237136c8bc
willyqin <qinwei206@gmail.com>
2020-12-20 14:13:57 +0800

    nr=0

--- run_train.py
+++ run_train.py
@@ -1,6 +1,6 @@
 import os
 
-def run_exp(batch_size=128, lr=0.1,noise_type='sym',noise_rate=0.4,gpu=0):
+def run_exp(batch_size=128, lr=0.1,noise_type='sym',noise_rate=0.0,gpu=0):
     the_cammand = 'python train.py' \
         +' --batch-size='+str(batch_size) \
         +' --lr='+str(lr) \
