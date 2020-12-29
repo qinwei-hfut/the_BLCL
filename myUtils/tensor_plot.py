46787e0129f591008f21d2a36900b4ca22902250
willyqin <qinwei206@gmail.com>
2020-12-29 19:03:13 +0800

    debug

--- myUtils/tensor_plot.py
+++ myUtils/tensor_plot.py
@@ -61,8 +61,7 @@ class TensorPlot():
                 plt.xlabel('epoch')
                 plt.legend()
 
-            print(os.path.join(self.saved_path,fig_key+'.pdf'))
-            plt.savefig(os.path.join(self.saved_path,fig_key+'.pdf'),format='pdf')
+            plt.savefig(os.path.join(self.saved_path,fig_key+'.jpg'),format='jpg')
             plt.close()
                 
 
