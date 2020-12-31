f031c4a267058d2e39ab9ff24757b382144c363a
willyqin <qinwei206@gmail.com>
2020-12-31 19:23:40 +0800

    run dict

--- run_train.py
+++ run_train.py
@@ -27,7 +27,7 @@ gpu=2
 
 # train_criterion = '{\"type\":\"MAE_loss\",\"args\":{}}'
 # train_criterion = '{"type":"MAE_loss","args":{}}'
-train_criterion = {"type":"MAE_loss","args":{}}
+train_criterion = str({"type":"MAE_loss","args":{}})
 # '{"a":1,"b":{"c":3,"d":4}}'
 pdb.set_trace()
 
