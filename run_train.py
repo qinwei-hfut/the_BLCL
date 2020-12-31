b6b185cd9bc126d5e2ca9d3c6bcf72bb2b050112
willyqin <qinwei206@gmail.com>
2020-12-31 11:07:35 +0800

    test run

--- run_train.py
+++ run_train.py
@@ -26,14 +26,14 @@ gpu=2
 
 
 train_criterion = '{"type":"MAE_loss","args":{}}'
-# pdb.set_trace()
 
 
 
 
 
 
-run_exp(trainer='trainer',arch='PreActResNet18',batch_size=128, lr=0.1,noise_type='asym',noise_rate=0.9,gpu=gpu,lr_schedule='120_200',train_criterion=train_criterion,epochs=250)
+
+run_exp(trainer='trainer',arch='PreActResNet18',batch_size=128, lr=0.1,noise_type='asym',noise_rate=0.9,gpu=gpu,lr_schedule='120_200',train_criterion='MAE_loss',epochs=250)
 # run_exp(trainer='trainer',arch='PreActResNet18',batch_size=128, lr=0.5,noise_type='asym',noise_rate=0.4,gpu=gpu,lr_schedule='250_400',train_criterion='MAE_loss',epochs=500)
 
 # run_exp(trainer='trainer',arch='PreActResNet18',batch_size=128, lr=1.0,noise_type='asym',noise_rate=0.4,gpu=gpu,lr_schedule='120_200',train_criterion='MAE_loss',epochs=250)
