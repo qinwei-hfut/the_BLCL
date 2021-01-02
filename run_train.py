7dc8f81c34dabad60cbffd4613885d16bf4857eb
willyqin <qinwei206@gmail.com>
2021-01-02 13:01:21 +0800

    dict 传入 test

--- run_train.py
+++ run_train.py
@@ -27,7 +27,7 @@ gpu=2
 
 # train_criterion = '{\"type\":\"MAE_loss\",\"args\":{}}'
 # train_criterion = '{"type":"MAE_loss","args":{}}'
-train_criterion = '{^^type^^:^^MAE_one_hot_loss^^,^^args^^:{^^num_classes^^:10}}'
+train_criterion = '{^^type^^:^^MAE_one_hot_loss^^,^^args^^:{^^num_classes^^:100}}'
 # train_criterion = str({"type":"MAE_loss","args":{}})
 # '{"a":1,"b":{"c":3,"d":4}}'
 # pdb.set_trace()
