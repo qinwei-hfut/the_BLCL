edfe7062d6566753ce8f72fe6c6368893992e478
willyqin <qinwei206@gmail.com>
2021-01-06 19:22:32 +0800

    debug

--- trainers/trainers.py
+++ trainers/trainers.py
@@ -1,6 +1,7 @@
 from .trainer import Trainer
 from .pytrainer import PyTrainer 
 from .meta_trainer import MetaTrainer
+import pdb
 
 def trainer(*args):
     return Trainer(*args)
@@ -8,5 +9,5 @@ def trainer(*args):
 def pytrainer(*args):
     return PyTrainer(*args)
 
-def meta_trainer(**args):
-    return MetaTrainer(**args)
+def meta_trainer(*args):
+    return MetaTrainer(*args)
