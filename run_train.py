1baf253a42c92e96ffa657358e1f9271d635b6c9
willyqin <qinwei206@gmail.com>
2021-01-06 18:50:24 +0800

    meta run

--- run_train.py
+++ run_train.py
@@ -32,11 +32,11 @@ gpu=0
 
 val_criterion = '\'{"type":"CE_loss","args":{}}\''
 # train_criterion = '\'{"type":"MAE_one_hot_loss","args":{}}\''
-train_criterion = '\'{"type":"CE_loss","args":{}}\''
+# train_criterion = '\'{"type":"CE_loss","args":{}}\''
 # train_criterion = '\'{"type":"SCE_loss","args":{"alpha":0.1,"beta":1.0}}\''
 # train_criterion = '\'{"type":"CE_MAE_loss","args":{"alpha":0.1,"beta":1.0}}\''
 # train_criterion = '\'{"type":"CE_LS_loss","args":{}}\''
-# train_criterion = '\'{"type":"Mixed_loss","args":{"alpha_ce":0.1,"alpha_rce":1.0,"alpha_mae":0.0,"alpha_mse":0.0}}\''
+train_criterion = '\'{"type":"Mixed_loss","args":{"alpha_ce":0.1,"alpha_rce":1.0,"alpha_mae":0.0,"alpha_mse":0.0}}\''
 
 arch = '\'{"type":"PreActResNet18","args":{"num_classes":10}}\''
 
@@ -52,7 +52,7 @@ meta_lr_scheduler = '\'{"type":"MultiStepLR","args":{"milestones":[40,80],"gamma
         
 
 
-run_exp(trainer='trainer',arch=arch,batch_size=128,optim=optim,meta_optim=meta_optim,lr_scheduler=lr_scheduler,meta_lr_scheduler=meta_lr_scheduler,noise_type='sym',noise_rate=0.9,epochs=250, dataset='cifar10', train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
+run_exp(trainer='meta_trainer',arch=arch,batch_size=128,optim=optim,meta_optim=meta_optim,lr_scheduler=lr_scheduler,meta_lr_scheduler=meta_lr_scheduler,noise_type='sym',noise_rate=0.9,epochs=250, dataset='cifar10', train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
 # run_exp(trainer='trainer',arch=arch,batch_size=128, lr=0.1,noise_type='sym',noise_rate=0.4,epochs=250,lr_schedule='120_200', dataset='cifar10', train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
 
 # run_exp(trainer='trainer',arch=arch,batch_size=128, lr=0.1,noise_type='sym',noise_rate=0.8,epochs=250,lr_schedule='120_200', dataset='cifar10', train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
--- trainers/trainers.py
+++ trainers/trainers.py
@@ -1,8 +1,12 @@
 from .trainer import Trainer
 from .pytrainer import PyTrainer 
+from .meta_trainer import MetaTrainer
 
 def trainer(*args):
     return Trainer(*args)
 
 def pytrainer(*args):
     return PyTrainer(*args)
+
+def meta_trainer(**args):
+    return MetaTrainer(**args)
