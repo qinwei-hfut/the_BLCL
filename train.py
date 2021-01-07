63d634352a44799501abbc4783295b7b8d2258a3
willyqin <qinwei206@gmail.com>
2021-01-07 15:27:57 +0800

    删除一些不用的args

--- train.py
+++ train.py
@@ -55,12 +55,6 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('--batch-size', default=128, type=int, metavar='N',
                     help='train batchsize')
-parser.add_argument('--lr', '--learning-rate', default=0.2, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--noise-type', '--nt',default='sym',type=str,choices=['sym','asym'])
 parser.add_argument('--noise-rate', '--nr',default=0.0, type=float)
 
