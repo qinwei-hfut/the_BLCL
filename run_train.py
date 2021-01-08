2c34b0dd9df26b4080d6851aa5cdfa6d7e990e3b
willyqin <qinwei206@gmail.com>
2021-01-08 16:30:04 +0800

    sym noise=0.8

--- run_train.py
+++ run_train.py
@@ -55,7 +55,7 @@ meta_lr_scheduler = '\'{"type":"MultiStepLR","args":{"milestones":[40,80],"gamma
         
 
 
-run_exp(trainer='meta_trainer',arch=arch,batch_size=128,optim=optim,meta_optim=meta_optim,lr_scheduler=lr_scheduler,meta_lr_scheduler=meta_lr_scheduler,noise_type='asym',noise_rate=0.2,epochs=140,warm_up_epochs=5,dataset='cifar10',warm_up_criterion=warm_up_criterion,train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
+run_exp(trainer='meta_trainer',arch=arch,batch_size=128,optim=optim,meta_optim=meta_optim,lr_scheduler=lr_scheduler,meta_lr_scheduler=meta_lr_scheduler,noise_type='sym',noise_rate=0.8,epochs=140,warm_up_epochs=5,dataset='cifar10',warm_up_criterion=warm_up_criterion,train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
 
 # run_exp(trainer='trainer',arch=arch,batch_size=128, lr=0.1,noise_type='sym',noise_rate=0.4,epochs=250,lr_schedule='120_200', dataset='cifar10', train_criterion=train_criterion,val_criterion=val_criterion,gpu=gpu)
 
