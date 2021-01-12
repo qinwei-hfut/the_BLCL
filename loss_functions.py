80b71b8528bbb32a48a7f82c358564efca5a8f09
willyqin <qinwei206@gmail.com>
2021-01-12 17:31:52 +0800

    NCEandRCE

--- loss_functions.py
+++ loss_functions.py
@@ -207,6 +207,19 @@ class ReverseCrossEntropy(torch.nn.Module):
         return self.scale * rce.mean()
 
 
+class NormalizedCrossEntropy(torch.nn.Module):
+    def __init__(self, num_classes, scale=1.0):
+        super(NormalizedCrossEntropy, self).__init__()
+        self.device = device
+        self.num_classes = num_classes
+        self.scale = scale
+
+    def forward(self, pred, labels):
+        pred = F.log_softmax(pred, dim=1)
+        label_one_hot = torch.nn.functional.one_hot(labels, self.num_classes).float().to(self.device)
+        nce = -1 * torch.sum(label_one_hot * pred, dim=1) / (- pred.sum(dim=1))
+        return self.scale * nce.mean()
+
 class NFLandRCE(torch.nn.Module):
     def __init__(self, alpha, beta, num_classes, gamma=0.5):
         super(NFLandRCE, self).__init__()
@@ -216,3 +229,14 @@ class NFLandRCE(torch.nn.Module):
 
     def forward(self, pred, labels):
         return self.nfl(pred, labels) + self.rce(pred, labels)
+
+
+class NCEandRCE(torch.nn.Module):
+    def __init__(self, alpha, beta, num_classes):
+        super(NCEandRCE, self).__init__()
+        self.num_classes = num_classes
+        self.nce = NormalizedCrossEntropy(scale=alpha, num_classes=num_classes)
+        self.rce = ReverseCrossEntropy(scale=beta, num_classes=num_classes)
+
+    def forward(self, pred, labels):
+        return self.nce(pred, labels) + self.rce(pred, labels)
