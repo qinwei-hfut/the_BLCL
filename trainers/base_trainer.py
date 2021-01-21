400f6196437025f45e48dc6c26ec295378bb1d1c
willyqin <qinwei206@gmail.com>
2021-01-21 12:48:29 +0800

    mute trace

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -23,7 +23,7 @@ class BaseTrainer(torch.nn.Module):
         self.val_loader = data.DataLoader(datasets[self.args.split_dataset['valset']],batch_size=self.args.batch_size,shuffle=False,num_workers=4)
         self.test_loader = data.DataLoader(datasets[self.args.split_dataset['testset']],batch_size=self.args.batch_size,shuffle=False,num_workers=4)
         self.meta_loader = data.DataLoader(datasets[self.args.split_dataset['metaset']],batch_size=self.args.batch_size,shuffle=True,num_workers=4)
-        pdb.set_trace()
+        # pdb.set_trace()
         self.model = model
         self.logger = logger
         
