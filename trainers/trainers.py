8ab5c5764be6cd75e7eb481c3d022ee6d828bac0
willyqin <qinwei206@gmail.com>
2021-01-27 08:03:17 +0800

    trainers中t g

--- trainers/trainers.py
+++ trainers/trainers.py
@@ -1,6 +1,7 @@
 from .trainer import Trainer
 from .pytrainer import PyTrainer 
 from .meta_trainer import MetaTrainer
+from .meta_layer_trainer import MetaLayerTrainer
 import pdb
 
 def trainer(*args):
@@ -11,3 +12,6 @@ def pytrainer(*args):
 
 def meta_trainer(*args):
     return MetaTrainer(*args)
+
+def meta_layer_trainer(*args):
+    return MetaLayerTrainer(*args)
