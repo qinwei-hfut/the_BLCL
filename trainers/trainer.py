b6c500db587c3442b50a6f73cd2811423a2b6621
willyqin <qinwei206@gmail.com>
2021-02-04 21:52:38 +0800

    set_trace; print(batch_idx)

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -27,6 +27,7 @@ class Trainer(BaseTrainer):
         #     for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(progress):
         for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(self.train_loader):
             # progress.set_description_str(f'Train epoch {epoch}')
+            print(batch_idx)
             inputs, noisy_labels, soft_labels, gt_labels = inputs.cuda(),noisy_labels.cuda(),soft_labels.cuda(),gt_labels.cuda()
 
             outputs = self.model(inputs)
