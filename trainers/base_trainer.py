ed64967e9de336417bd0091e67d834306830c6e9
willyqin <qinwei206@gmail.com>
2021-02-04 22:44:25 +0800

    finetune no need print batch idx

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -245,10 +245,6 @@ class BaseTrainer(torch.nn.Module):
 
         for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(self.meta_loader):
 
-            if batch_idx % 100 == 0:
-                print(batch_idx)
-            if batch_idx == 500:
-                break
 
             inputs, noisy_labels, soft_labels, gt_labels = inputs.cuda(),noisy_labels.cuda(),soft_labels.cuda(),gt_labels.cuda()
 
