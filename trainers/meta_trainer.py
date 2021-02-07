3ccde9d1106e4574b7b1ba7427f697235aee963f
willyqin <qinwei206@gmail.com>
2021-02-07 12:18:00 +0800

    mini batch 1000

--- trainers/meta_trainer.py
+++ trainers/meta_trainer.py
@@ -63,10 +63,10 @@ class MetaTrainer(BaseTrainer):
         for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(self.train_loader):
             inner_inputs, inner_noisy_labels, inner_soft_labels, inner_gt_labels = inputs.cuda(),noisy_labels.cuda(),soft_labels.cuda(),gt_labels.cuda()
 
-            # if batch_idx % 400 == 0:
-            #     print(batch_idx)
-            print(batch_idx)
-            if batch_idx == 2:
+            if batch_idx % 400 == 0:
+                print(batch_idx)
+            # print(batch_idx)
+            if batch_idx == 1000:
                 break
             
             with higher.innerloop_ctx(self.model,self.optimizer,copy_initial_weights=False) as (fnet,diffopt):
