c5c518d7d0683a5f425d2e713324a18fb2baf2ab
willyqin <qinwei206@gmail.com>
2021-02-07 10:42:35 +0800

    small clean val

--- myUtils/process_small_train_data.py
+++ myUtils/process_small_train_data.py
@@ -26,7 +26,30 @@ def generate_small_clean_trainset(sample_per_class_clean_train):
                 shutil.copy(os.path.join(class_root_big,fnames[j]),os.path.join(class_root_small,fnames[j]))
 
 
-generate_small_clean_trainset(sample_per_class_clean_train=10)
+def generate_small_clean_valset(sample_per_class_clean_val):
+
+    # small clean trainset
+    root = "/sharedir/dataset"
+    big_clean_valset_path = os.path.join(root, "clean_val")
+    small_clean_valset_path = os.path.join(root,str(sample_per_class_clean_val)+'_small_clean_val')
+    # 不能够删除，因为多个代码并行运行，删除一个可能会导致另外一个无法加载数据
+    # if os.path.exists(small_clean_trainset_path):
+    #     shutil.rmtree(small_clean_trainset_path)
+    # os.makedirs(small_clean_trainset_path)
+    for i in range(14):
+        class_root_small = os.path.join(small_clean_valset_path,str(i))
+        os.makedirs(class_root_small)
+
+        class_root_big = os.path.join(big_clean_valset_path,str(i))
+        for root, _, fnames in sorted(os.walk(class_root_big, followlinks=True)):
+            random.shuffle(fnames)
+            for j in range(sample_per_class_clean_val):
+                shutil.copy(os.path.join(class_root_big,fnames[j]),os.path.join(class_root_small,fnames[j]))
+
+
+
+# generate_small_clean_trainset(sample_per_class_clean_train=10)
+generate_small_clean_valset(sample_per_class_clean_val=10)
 
         
 
