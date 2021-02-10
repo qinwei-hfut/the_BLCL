7efc7a193eb486e5c0d6674a8bce281f875dbd31
willyqin <qinwei206@gmail.com>
2021-02-10 16:12:46 +0800

    add Neg_Trainer

--- trainers/trainers.py
+++ trainers/trainers.py
@@ -2,6 +2,7 @@ from .trainer import Trainer
 from .pytrainer import PyTrainer 
 from .meta_trainer import MetaTrainer
 from .meta_layer_trainer import MetaLayerTrainer
+from .neg_trainer import Neg_Trainer
 import pdb
 
 def trainer(*args):
@@ -15,3 +16,6 @@ def meta_trainer(*args):
 
 def meta_layer_trainer(*args):
     return MetaLayerTrainer(*args)
+
+def neg_trainer(*args):
+    return Neg_Trainer(*args)
