24166f36a09a7f8140cc190abc38a02e0557de89
willyqin <qinwei206@gmail.com>
2021-02-14 16:29:35 +0800

    init from trainer.py

---
+++ trainers/DoubleFC_trainer.py
@@ -0,0 +1,72 @@
+import torchvision
+import torch
+import numpy as np
+from .base_trainer import BaseTrainer
+import torch.utils.data as data
+from tqdm import tqdm
+import os
+from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+import pdb
+
+
+class Trainer(BaseTrainer):
+    def __init__(self,model,datasets,logger,resuls_saved_path,args):
+        super().__init__(model,datasets,logger,resuls_saved_path,args)
+
+
+    def _train_epoch(self):
+        self.model.train()
+        losses = AverageMeter()
+        Ntop1 = AverageMeter()
+        Ntop5 = AverageMeter()
+
+        Ctop1 = AverageMeter()
+        Ctop5 = AverageMeter()
+
+        # with tqdm(self.train_loader) as progress:
+        #     for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(progress):
+        for batch_idx, (inputs, noisy_labels, soft_labels, gt_labels, index) in enumerate(self.train_loader):
+            # progress.set_description_str(f'Train epoch {epoch}')
+            # print(batch_idx)
+            if batch_idx % 400 == 0:
+                print(batch_idx)
+            if batch_idx == 1000:
+                break
+            inputs, noisy_labels, soft_labels, gt_labels = inputs.cuda(),noisy_labels.cuda(),soft_labels.cuda(),gt_labels.cuda()
+
+            outputs = self.model(inputs)
+
+            loss = self.train_criterion(outputs,noisy_labels)
+            # pdb.set_trace()
+
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            # ################ print log
+            # for group in self.optimizer.param_groups:
+            #     for p in group['params']:
+            #         print(p.grad)
+
+
+            self.optimizer.step()
+
+            Nprec1, Nprec5 = accuracy(outputs,noisy_labels,topk=(1,5))
+            Cprec1, Cprec5 = accuracy(outputs,gt_labels,topk=(1,5))
+            losses.update(loss.item(), inputs.size(0))
+            Ntop1.update(Nprec1.item(), inputs.size(0))
+            Ntop5.update(Nprec5.item(), inputs.size(0))
+            Ctop1.update(Cprec1.item(), inputs.size(0))
+            Ctop5.update(Cprec5.item(), inputs.size(0))
+
+        val_loss, val_acc1, val_acc5 = self._val_epoch()
+        test_loss, test_acc1, test_acc5 = self._test_epoch()
+
+        log = {'train_loss':losses.avg,
+            'train_N_acc_1':Ntop1.avg,
+            'train_C_acc_1':Ctop1.avg,
+            'val_loss':val_loss,
+            'val_acc_1':val_acc1,
+            'test_loss':test_loss,
+            'test_acc_1':test_acc1}
+        return log
+    
