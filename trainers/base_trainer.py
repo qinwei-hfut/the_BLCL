fc71a83cdccaed5c3a6498b2d590624e3326b4fd
willyqin <qinwei206@gmail.com>
2021-03-07 16:28:03 +0800

    debug

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -95,8 +95,8 @@ class BaseTrainer(torch.nn.Module):
         # for epoch in tqdm(range(self.args.epochs),decs='Total progress: '):
         for epoch in range(self.args.epochs):
             self.epoch = epoch
-            # if 'meta' in self.args.trainer:  
-            self._plot_loss_weight()       
+            if 'meta' in self.args.trainer:  
+                self._plot_loss_weight()       
             if self.epoch < self.warm_up_epochs:
                 print('warm_epoch: '+str(self.epoch))
                 results = self._warm_up()
