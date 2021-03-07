212dd34046ff18ae056118482f353599bca59069
willyqin <qinwei206@gmail.com>
2021-03-07 16:56:19 +0800

    detach

--- trainers/DoubleFC_trainer.py
+++ trainers/DoubleFC_trainer.py
@@ -55,11 +55,11 @@ class DoubleFC_Trainer(BaseTrainer):
 
             # predictions = self.softmax(output_main)
             full_batch_index = torch.tensor([i for i in range(gt_labels.size(0))],device='cuda')
-            output_main = output_main.detach()
+            # output_main.detach_()
             output_main[full_batch_index,gt_labels] = torch.tensor(float('-inf'),device='cuda')
             _,negative_label = output_main.max(dim=1)
             # pdb.set_trace()
-            loss_2 = self.train_criterion(output_2,negative_label)   #TODO .mean()?
+            loss_2 = self.train_criterion(output_2,negative_label.detach())   #TODO .mean()?
             
             self.optimizer.zero_grad()
             loss_2.backward()
